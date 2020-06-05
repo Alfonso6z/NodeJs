e0a8e08780ed41254f26628cd37b94382329dc92
Alfonso6z <agzdn666@gmail.com>
2020-06-04 22:23:55 -0500

    .gitignore aplicación multiplicar, yargs colores

--- 03_bases_node/app.js
+++ 03_bases_node/app.js
@@ -7,13 +7,31 @@
 // console.log(module);  es un objeto global que esta disponible a lo largo de la app
 // console.log(process.argv);
 
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1];
+// let argv2 = process.argv;
+// console.log(argv.base);
+// console.log(argv.limite);
 
-const { crearArchivo } = require('./multiplicar/multiplicar');
+// let parametro = argv[2];
+// let base = parametro.split('=')[1];
 
+const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
 
-crearArchivo(base)
-    .then(archivo => console.log(`Archivo creado: ${archivo}`))
-    .catch(e => console.log(e));
+const { crearArchivo, listar } = require('./multiplicar/multiplicar');
+let commando = argv._[0];
+
+switch (commando) {
+    case 'listar':
+        listar(argv.base, argv.limite)
+            .then(base => console.log('Correcto'))
+            .catch(e => console.log('mal'));
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado:`, colors.rainbow(archivo)))
+            .catch(e => console.log(e));
+        break;
+    default:
+        console.log('comando no reconocido');
+
+}
--- 03_bases_node/multiplicar/multiplicar.js
+++ 03_bases_node/multiplicar/multiplicar.js
@@ -1,28 +1,54 @@
 const fs = require('fs');
+const colors = require('colors');
 
-let crearArchivo = (base) => { /*  podria  agregarce al module asi **module.exports.crearArchivo = (base) =>{...} */
+let listar = (base, limite = 10) => {
+    return new Promise((resolve, reject) => {
+        if (!Number(base)) {
+            reject(`El valor introducido ${base} no es un número`);
+            return;
+        } else if (!Number(limite)) {
+            reject(`El valor introducido ${limite} no es un número`);
+            return;
+        }
+        console.log(`=====================`.green);
+        console.log(`Tala del ${base}`.green);
+        console.log(`=====================`.green);
+
+        for (let i = 1; i <= limite; i++) {
+            console.log(`${base} * ${i} = ${base*i}`.rainbow);
+        }
+    });
+}
+
+
+let crearArchivo = (base, limite = 10) => { /*  podria  agregarce al module asi **module.exports.crearArchivo = (base) =>{...} */
     return new Promise((resolve, reject) => {
 
         if (!Number(base)) {
             reject(`El valor introducido ${base} no es un número`);
             return;
         }
+        if (!Number(limite)) {
+            reject(`El valor introducido ${limite} no es un número`);
+            return;
+        }
 
         let data = '';
 
-        for (let i = 1; i <= 10; i++) {
+        for (let i = 1; i <= limite; i++) {
             data += `${base} * ${i} = ${base*i}\n`;
         }
 
-        fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+        fs.writeFile(`tablas/tabla-${base}-al-${limite}.txt`, data, (err) => {
             if (err)
                 reject(err)
             else
-                resolve(`tabal-${base}`)
+                resolve(`tabla-${base}-al-${limite}`)
         });
     });
 }
 
 module.exports = {
-    crearArchivo
+    crearArchivo,
+    listar
 }
