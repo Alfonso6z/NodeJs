6e402353631039d6c3ba22f947820dee66c4ffad
Alfonso6z <agzdn666@gmail.com>
2020-06-16 17:23:07 -0500

    restserver usuario, mongo altlas
    Definir los alcances de nuestro RESTServer
    CRUD
    Encriptación de contraseñas
    Validaciones personalizadas
    Creación de roles
    Conexiones con MLAB
    Despliegue de base de datos en la nube
    Conexión con Robo 3T con base de datos en la nube
    Configuración de variables de entorno
    Borrado de archivos
    Eliminado físico de la base de datos
    Eliminación por estado en un campo de la colección

--- 07_restserver/server/server.js
+++ 07_restserver/server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser')
 
@@ -8,41 +10,18 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use(require('./routes/usuario'))
 
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-});
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({ /* satus code */
-            ok: false,
-            mensaje: 'El nombre es Necesario',
-            err: '8======D'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+    (err, res) => {
+        if (err) throw err;
+        console.log('Base de datos ONLINE');
     });
-});
 
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando el puerto: `, process.env.PORT);
