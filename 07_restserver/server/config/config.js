88f73dbceb5d2a32d6d9fde32ddb6c984e30ac60
Alfonso6z <agzdn666@gmail.com>
2020-06-18 19:35:47 -0500

    Google SingIn
    Generar API Key de Google
    Generar API Secret
    Usar librerías de Google para la validación de tokens
    Tips importantes en PostMan
    Despliegues a Heroku
    Uso del Google SignIn en el Front-End
    Crear usuarios personalizados en base a respuestas de Google

--- 07_restserver/server/config/config.js
+++ 07_restserver/server/config/config.js
@@ -39,3 +39,9 @@ if (process.env.NODE_ENV === 'dev') {
 }
 
 process.env.URLDB = urlDB;
+
+/**
+ * Google Client ID
+ */
+
+process.env.CLIENT_ID = process.env.CLIENTE_I || '779516978679-rfa5d0coskkf9cobvji3t3cb1g8qrdo0.apps.googleusercontent.com'
