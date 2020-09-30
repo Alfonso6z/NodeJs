cd15a65db29db43740cb637f3b5accfbd25a7a87
Alfonso6z <agzdn666@gmail.com>
2020-09-30 16:41:25 -0500

    Respaldo Chat

---
+++ 10_socket_chat/server/classes/usuarios.js
@@ -0,0 +1,45 @@
+class Usuarios {
+
+    constructor() {
+        this.personas = [];
+    }
+
+    agregarPersona(id, nombre, sala) {
+        let persona = { id, nombre, sala };
+
+        this.personas.push(persona);
+
+        return this.personas;
+    }
+
+    getPersonaId(id) {
+        let persona = this.personas.filter(persona => persona.id === id)[0];
+        return persona;
+    }
+
+    getPersonas() {
+        return this.personas;
+    }
+
+    getPersonasPorSalas(sala) {
+
+        let personasEnSala = this.personas.filter(persona => persona.sala === sala);
+        return personasEnSala;
+    }
+
+    borrarPersona(id) {
+
+        let personaBorrada = this.getPersonaId(id);
+
+        this.personas = this.personas.filter(persona => {
+            return persona.id != id;
+        });
+
+        return personaBorrada;
+    }
+
+}
+
+module.exports = {
+    Usuarios,
+}
